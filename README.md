d8804ef5238330cf6f39111bfe4b39afb94ed08b
silvia-YQY <545067444@qq.com>
2018-03-21 10:50:24 +0800

    天气npm

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Weather-npm
-npm-weather
+
+这是一个天气查询应用
+
+# 使用方法
+
+```
+npm install yqy-weather -g
+```
+
+```
+weather guangzhou //需要使用城市拼音，不输入城市，将显示当前IP地址的天气
+```
+
+以上查询，将会显示您查询的城市当前天气
--- index.js
+++ index.js
@@ -6,22 +6,40 @@ var axios = require("axios")
 var data = {}
 
 if(process.argv[2]){
-    data.params = {
-        city_name: process.argv[2]
-    }
+    //查询城市id
+    axios.get('https://weixin.jirengu.com/weather/cityid?',{
+        params:{
+            location:process.argv[2]
+        }
+    }).then(function(response){
+        url= 'https://weixin.jirengu.com/weather'+'/now'
+        data.params = {
+            'cityid':response.data.results[0].id
+        }
+        getWeather(url,data) 
+    }).catch(function(error){
+        console.log(error)
+    })
+}else{
+    url = 'https://weixin.jirengu.com/weather'
+    getWeather(url) 
 }
 
-axios.get('http://weixin.jirengu.com/weather',data)
-    .then(function(response){
-        
+function getWeather(url,data){
+
+    axios.get(url,data)
+        .then(function(response){
+            
         var Nowweather = response.data.weather[0].now
+        console.log('城市',response.data.weather[0].city_name)
         console.log('日期：',response.data.weather[0].last_update)
         console.log('温度：',Nowweather.temperature)
         console.log('能见度：',Nowweather.visibility)
         console.log('风向：',Nowweather.wind_direction)
-        console.log('空气质量（PM2.5）：',Nowweather.air_quality.city.pm25)
+        console.log('空气质量（PM2.5）：',Nowweather.air_quality.city.pm25,Nowweather.air_quality.city.quality)
 
     })
     .catch(function(error){
         console.log(error)
     })
+}
